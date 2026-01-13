84162ef7f0c0d21f948db52261015a68c3d845ae
Anurag Singh <contact@anurag.sh>
2026-01-13 16:20:24 +0800

    send events

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -1,8 +1,10 @@
 use crate::{
+    get_average_for_last_n_minutes,
     goodwe::{self, GoodWeSemsAPI},
     weather::{self, WeatherAPI},
 };
 use futures::FutureExt;
+use serde::Serialize;
 use sqlx::PgPool;
 use std::{panic::AssertUnwindSafe, time::Duration};
 
@@ -11,6 +13,7 @@ pub struct BackgroundTask {
     pool: PgPool,
     solar_api: GoodWeSemsAPI,
     weather_api: WeatherAPI,
+    http_client: reqwest::Client,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -21,8 +24,24 @@ pub enum BackgroundTaskError {
     WeatherAPI(#[from] weather::WeatherAPIError),
     #[error("a database error occurred: {0}")]
     Database(#[from] sqlx::Error),
+    #[error("a http error occurred: {0}")]
+    Http(#[from] reqwest::Error),
     #[error("unknown error occurred: {0}")]
     Unknown(#[from] anyhow::Error),
+}
+
+#[derive(Serialize)]
+pub struct SolarIngestAvgPayload {
+    pub mins_15: Option<f64>,
+    pub mins_60: Option<f64>,
+    pub mins_180: Option<f64>,
+}
+
+#[derive(Serialize)]
+pub struct SolarIngestPayload {
+    pub current_kwh: f64,
+    pub average_kwh: SolarIngestAvgPayload,
+    pub uv_level: Option<f64>,
 }
 
 impl BackgroundTask {
@@ -31,6 +50,7 @@ impl BackgroundTask {
             pool,
             solar_api,
             weather_api,
+            http_client: reqwest::ClientBuilder::new().build().unwrap(),
         }
     }
 
@@ -82,6 +102,39 @@ impl BackgroundTask {
                     )
                     .execute(&self_cloned.pool)
                     .await?;
+
+                    if let Some(home_gateway_api_base) = std::env::var("HOME_GATEWAY_BASE_URL").ok()
+                    {
+                        let url = format!("{home_gateway_api_base}/v1/ingest/solar");
+                        let api_key =
+                            std::env::var("HOME_GATEWAY_API_KEY").unwrap_or_else(|_| "".to_owned());
+
+                        // FIXME: expensive
+                        let avg_15_mins =
+                            get_average_for_last_n_minutes(15, &self_cloned.solar_api).await?;
+                        let avg_1_hour =
+                            get_average_for_last_n_minutes(60, &self_cloned.solar_api).await?;
+                        let avg_3_hours =
+                            get_average_for_last_n_minutes(180, &self_cloned.solar_api).await?;
+
+                        let response = self_cloned
+                            .http_client
+                            .post(url)
+                            .header("X-Api-Key", api_key)
+                            .json(&SolarIngestPayload {
+                                current_kwh: kwh,
+                                average_kwh: SolarIngestAvgPayload {
+                                    mins_15: avg_15_mins,
+                                    mins_60: avg_1_hour,
+                                    mins_180: avg_3_hours,
+                                },
+                                uv_level,
+                            })
+                            .send()
+                            .await?;
+
+                        tracing::info!("home-gateway response: {}", response.status());
+                    };
 
                     Ok::<(), BackgroundTaskError>(())
                 };
